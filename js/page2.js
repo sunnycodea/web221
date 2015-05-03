05ad9184723a65a58732228219d3400ba3a98899
ligexiao <helloyabogmail.com>
2015-05-03 13:58:31 +0800

    基本部分：旅游主题+路线

--- js/page2.js
+++ js/page2.js
@@ -71,6 +71,17 @@ $(document).ready(function(){
             '8':{"name":"title8"},
             '9':{"name":"title9"},
         };
+    var data_set = {'0':{'title':'title1','brief':'brief1','price':'1111'},
+                    '1':{'title':'title2','brief':'brief2','price':'2222'},
+                    '2':{'title':'title3','brief':'brief3','price':'3333'},
+                    '3':{'title':'title4','brief':'brief4','price':'4444'},
+                    '4':{'title':'title5','brief':'brief5','price':'5555'},
+                    '5':{'title':'title6','brief':'brief6','price':'6666'},
+                    '6':{'title':'title7','brief':'brief7','price':'7777'},
+                    '7':{'title':'title8','brief':'brief8','price':'8888'},
+                    '8':{'title':'title9','brief':'brief9','price':'9999'},
+                    '9':{'title':'title10','brief':'brief10','price':'1010'}
+            };
     $('#pagination-demo').twbsPagination({
         json: jsond,
         totalPages: 9,
@@ -82,16 +93,83 @@ $(document).ready(function(){
         last: '>',
         loop: true,
         onPageClick: function (event, page) {
+            $(".theme").remove();
+            /*
             $(".theme").each(function(key, obj){
                 pgnumber = $(obj).attr('pgnum');
-                pagetmp=page%2+1;
+                console.log(pgnumber);
+                //pagetmp=page%2+1;
                 if(pagetmp==pgnumber){
                     $(obj).css('display','block');
                 }else{
                     $(obj).css('display','none');
                 }
-            });
+                $('.mouseky').each(function(key, obj){
+                    var myVar = $(obj).find('.price').html('hello') ;
+                     //console.log(myVar);
+                     
+                });
+               
+            });*/ 
+
+            var rditems = createThemeDivItems(page,data_set);
+            $("#hehe").after(rditems);
         }
     });
 
+    var createThemeDivItems = function ( page,data_set) {
+        var themeContainer = $('<div></div>');
+        themeContainer.addClass("theme");
+        themeContainer.attr('pnum', page);
+
+        themeContainer.append(createRouteDivItems(page,10,data_set));
+        return themeContainer;
+    };
+    
+    var createRouteDivItems = function (page, route_num,data_set) {
+        var $divItems = $();
+
+        for (var i=0;i<route_num;i++)
+        {
+            if(i%2==0){
+              var  rowContent = $('<div></div>');
+            }
+            rowContent = createRouteItem(rowContent,data_set[i].title+page,data_set[i].brief+page,data_set[i].price*page)
+            $divItems = $divItems.add(rowContent);
+        }
+        return $divItems;
+        
+    };
+    var createRouteItem = function (rowContent,title,brief,price){
+        var mouseContent = $('<div></div>'),
+            tfoContent = $('<div></div>'),
+            titleContent = $('<h4></h4>'),
+            briefContent = $('<p></p>'),
+            pfoContent = $('<div></div>'),
+            priceContent = $('<p></p>'),
+            itemText = null;
+
+        rowContent.addClass("row yabohe row-margin-top clearfix");
+
+        mouseContent.addClass("col-md-6 column mouseky");
+
+        tfoContent.addClass("col-md-6 column info" );
+        titleContent.addClass("title" ).html(title);
+        briefContent.addClass("brief" ).html(brief);
+        tfoContent.append(titleContent);
+        tfoContent.append(briefContent);
+
+        pfoContent.addClass("col-md-6 column info" );
+        priceContent.addClass("price" ).html(price);
+        pfoContent.append(priceContent);
+
+        mouseContent.append(tfoContent);
+        mouseContent.append(pfoContent);
+
+        rowContent.append(mouseContent);
+        return rowContent;
+    }
+
+    var rditems = createThemeDivItems(1,data_set);
+    $("#hehe").after(rditems);
 });
