b1c0c6c3952d2bf5a73170596719a6ac29376aa8
Grissom <luckscx@gmail.com>
2015-05-03 19:39:56 +0800

    fix

--- wh_svr/yii/framework/wh_db/protected/config/database.php
+++ wh_svr/yii/framework/wh_db/protected/config/database.php
@@ -4,9 +4,9 @@
 return array(
 	'connectionString' => 'mysql'.dirname(__FILE__).'/../data/schema.mysql.sql',
 	// uncomment the following lines to use a MySQL database
-	'connectionString' => 'mysql:host=10.24.248.90;dbname=dbwh',
+	'connectionString' => 'mysql:host=127.0.0.1;dbname=wh_svr',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => '123456',
 	'charset' => 'utf8',
 );
